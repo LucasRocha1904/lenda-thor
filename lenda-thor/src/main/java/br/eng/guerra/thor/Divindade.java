fc99ad1325205d240b032cf42949dcce31d828de
unknown <lucasalmroch19@gmail.com>
2025-08-25 17:24:17 -0300

    mais classes

---
+++ lenda-thor/src/main/java/br/eng/guerra/thor/Divindade.java
@@ -0,0 +1,5 @@
+package br.eng.guerra.thor;
+
+public class Divindade {
+
+}
---
+++ lenda-thor/src/main/java/br/eng/guerra/thor/Mortal.java
@@ -0,0 +1,12 @@
+package br.eng.guerra.thor;
+
+import java.sql.Date;
+
+public abstract class Mortal extens Ser{
+
+    private Date nascimento;
+    private Divindade hospeda;
+
+    
+
+}
